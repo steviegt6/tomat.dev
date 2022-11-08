d65258961856caea5edf8682af3be8be6621cb91
Tomat <xxlennygamerxx@gmail.com>
2022-11-07 22:27:28 -0800

    Add app Shell component.

---
+++ src/components/Shell/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "./shell.module.css";
+
+export interface ShellProps {
+  children: React.ReactNode;
+}
+
+export default function Shell({ children }: ShellProps) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Shell from "components/Shell";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Shell>
+      <Component {...pageProps} />
+    </Shell>
+  );
 }
