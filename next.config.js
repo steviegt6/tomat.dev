3ee06230dc20a9bc4a711d5bb1bbdf4080523e2d
Tomat <xxlennygamerxx@gmail.com>
2022-12-29 20:32:52 -0800

    gragh

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
 };
 
 module.exports = nextConfig;
