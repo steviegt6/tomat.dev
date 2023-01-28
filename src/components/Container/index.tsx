935240933463b799e385fce5999e7b45e2b2f427
Tomat <xxlennygamerxx@gmail.com>
2023-01-28 15:47:21 -0800

    Preparing for excess meta.

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,20 +1,22 @@
 import Head from "next/head";
 import { PropsWithChildren } from "react";
 
-export type BasicLayoutProps = PropsWithChildren<{
+export type ContainerProps = PropsWithChildren<{
     title: string;
     description: string;
 }>;
 
-export default function Container({ title, description, children }: BasicLayoutProps) {
+export default function Container(props: ContainerProps) {
+    const { children, ...meta } = props;
+
     return (
         <>
             <Head>
                 {/* Using {title} directly causes hydration errors? */}
                 {/* Counts as giving it more than one child element... */}
                 {/* Other quirks include appending <!-- --> somehow?? */}
-                <title>{`${title} | tomat.dev`}</title>
-                <meta name="description" content={description} />
+                <title>{`${meta.title} | tomat.dev`}</title>
+                <meta content={meta.description} name="description" />
             </Head>
             {children}
         </>
