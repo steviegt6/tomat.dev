105c37c547e6b89da086cbb2633056b0894f73a6
Tomat <xxlennygamerxx@gmail.com>
2023-01-27 22:43:57 -0800

    Goodbye, frameBorder.

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -17,7 +17,6 @@ export default function Test() {
                             height="315"
                             src="https://www.youtube.com/embed/E4WlUXrJgy4?autoplay=1&mute=1"
                             title="YouTube video player"
-                            frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowFullScreen
                         ></iframe>
