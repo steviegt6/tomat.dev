7f3eff5f6d713c7f70f53372f723314c06b004e5
Tomat <xxlennygamerxx@gmail.com>
2023-01-28 15:52:20 -0800

    Canonical.

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { PropsWithChildren } from "react";
+
+export const canonical = "https://tomat.dev";
 
 export type ContainerProps = PropsWithChildren<{
     title: string;
@@ -8,12 +11,14 @@ export type ContainerProps = PropsWithChildren<{
 
 export default function Container(props: ContainerProps) {
     const { children, ...meta } = props;
+    const router = useRouter();
 
     return (
         <>
             <Head>
                 <title>{`${meta.title} | tomat.dev`}</title>
                 <meta content={meta.description} name="description" />
+                <link rel="canonical" href={`${canonical}${router.asPath}`} />
             </Head>
             {children}
         </>
