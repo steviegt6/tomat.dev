cba6f1823f6bc0336999f9ffce5a7086f54ba02a
Tomat <xxlennygamerxx@gmail.com>
2023-01-28 17:09:31 -0800

    Add Twitter meta.

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -7,20 +7,38 @@ export const canonical = "https://tomat.dev";
 export type ContainerProps = PropsWithChildren<{
     title: string;
     description: string;
+    image?: string;
 }>;
 
 export default function Container(props: ContainerProps) {
     const { children, ...meta } = props;
     const router = useRouter();
 
+    meta.title = `${meta.title} | tomat.dev`;
+
     return (
         <>
             <Head>
-                <title>{`${meta.title} | tomat.dev`}</title>
+                <title>{meta.title}</title>
                 <meta content={meta.description} name="description" />
                 <link rel="canonical" href={`${canonical}${router.asPath}`} />
+                <TwitterMeta {...meta} />
             </Head>
             {children}
+        </>
+    );
+}
+
+type MetaProps = Omit<ContainerProps, "children">;
+
+function TwitterMeta({ description, image }: MetaProps) {
+    return (
+        <>
+            <meta name="twitter:site" content="@tomatdev" />
+            <meta name="twitter:creator" content="@tomatdev" />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:image" content={image || "/banner.png"} />
         </>
     );
 }
