e77716ed6dc771cef314c9ea694bd455ab482c69
Tomat <xxlennygamerxx@gmail.com>
2023-01-31 19:35:18 -0800

    Socials on main page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,19 @@ export default function Home() {
     return (
         <LogoContainer width={250} height={250} interactable clickable>
             <Container title={title} description={description}>
-                <p>Goober</p>
+                <h1>Stevie, Tomat, and more...</h1>
+                <hr />
+                <br />
+                <p>Hobbyist programmer and game modder. You may have seen me around, or maybe not.</p>
+                <br />
+                <p>Let&apos;s make this quick... relevant links:</p>
+                <p>
+                    - my <a href="https://github.com/steviegt6/">GitHub</a>,
+                    <br />- my <a href="https://discordapp.com/users/269903979582849024">Discord</a> (Tomat#0001),
+                    <br />- my <a href="https://youtube.com/c/tomatophile/">YouTube</a>,
+                    <br />- my <a href="https://twitter.com/steviegt6">Twitter</a>,
+                    <br />
+                </p>
             </Container>
         </LogoContainer>
     );
