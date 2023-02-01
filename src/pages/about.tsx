ef0d511c4fa520cef70b3e7944cc4be7dfa591c9
Tomat <xxlennygamerxx@gmail.com>
2023-01-31 19:26:35 -0800

    guh guh guh about me page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,7 @@
 import Container from "@/components/Container";
-import LogoContainer from "@/components/LogoContainer";
 
 const title = "/about";
-const description = "superficial greetings to you all";
+const description = "superficial salutations!";
 
 export default function About() {
     return (
@@ -10,7 +9,27 @@ export default function About() {
             <h1>About</h1>
             <hr />
             <br />
-            <p>brhu</p>
+            <p>
+                Hello, I&apos;m <strong>Stevie</strong>, known in some circles as <strong>Tomat</strong>{" "}
+                <s>(as well as convicted tomatophile)</s>.
+            </p>
+            <br />
+            <p>
+                Among other—probably less notable—things, I am a programmer and game modder. I enjoy reverse-enginnering
+                and messing with software in ways I probably shouldn&apos;t.
+            </p>
+            <p>My exploits are plentiful and range in quality.</p>
+            <br />
+            <p>
+                I know—and am confident in—<strong>C#</strong>; <strong>Java</strong>;{" "}
+                <strong>HTML, CSS, &amp; JS</strong>; and <strong>TypeScript</strong>, as well as many other languages
+                of varying knowledge.
+            </p>
+            <br />
+            <p>
+                Whether willingly or not, I've become familiar with the overarching <strong>.NET Ecosystem</strong>,{" "}
+                <strong>MSBuild</strong>, <strong>Node.js</strong>, and more.
+            </p>
         </Container>
     );
 }
